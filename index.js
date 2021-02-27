58eeb25bc94750f3df95c792bd5d76994eb1fd06
Teerasej <teerasej@gmail.com>
2021-02-27 22:32:27 +0700

    use appropriate way to separate route

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = 3000
 
 app.use(express.json());
 app.use(passport.initialize());
+app.use(routeNotes);
 
 
 
--- routes/notes.js
+++ routes/notes.js
@@ -1,67 +1,72 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+
 const { ObjectId } = require('mongodb');
 const dbConnection = require('../db');
 const dbName = 'nfmongop';
 
 
-const settingRoute = (app) => {
-    // read
-    app.get('/notes', async (req, res) => {
+// read
+router.get('/notes', async (req, res) => {
 
-        let client = await dbConnection.connect()
-        let collection = client.db(dbName).collection('notes')
-        let result = await collection.find().limit(10).toArray()
-        client.close()
+    let client = await dbConnection.connect()
+    let collection = client.db(dbName).collection('notes')
+    let result = await collection.find().limit(10).toArray()
+    client.close()
 
-        res.json(result)
-    })
+    res.json(result)
+})
 
-    // create
-    app.post('/notes', async (req, res) => {
+// create
+router.post('/notes', async (req, res) => {
 
-        let newNote = req.body
+    let newNote = req.body
 
-        let client = await dbConnection.connect()
-        let collection = client.db(dbName).collection('notes')
-        let result = await collection.insertOne(newNote);
-        client.close()
+    let client = await dbConnection.connect()
+    let collection = client.db(dbName).collection('notes')
+    let result = await collection.insertOne(newNote);
+    client.close()
 
-        res.json(result)
-    })
+    res.json(result)
+})
 
-    // update
-    app.put('/notes', async (req, res) => {
+// update
+router.put('/notes', async (req, res) => {
 
-        let targetNote = req.body
+    let targetNote = req.body
 
-        let client = await dbConnection.connect()
-        let collection = client.db(dbName).collection('notes')
+    let client = await dbConnection.connect()
+    let collection = client.db(dbName).collection('notes')
 
-        let filter = { _id: ObjectId(targetNote._id) }
-        let query = {
-            $set: { first_name: targetNote.first_name }
-        }
+    let filter = { _id: ObjectId(targetNote._id) }
+    let query = {
+        $set: { first_name: targetNote.first_name }
+    }
 
-        let result = await collection.updateOne(filter, query)
-        client.close()
+    let result = await collection.updateOne(filter, query)
+    client.close()
 
-        res.json(result);
-    })
+    res.json(result);
+})
 
-    // delete
-    app.delete('/notes', async (req, res) => {
+// delete
+router.delete('/notes', async (req, res) => {
 
-        let targetNote = req.body
+    let targetNote = req.body
 
-        let client = await dbConnection.connect()
-        let collection = client.db(dbName).collection('notes')
+    let client = await dbConnection.connect()
+    let collection = client.db(dbName).collection('notes')
 
-        let filter = { _id: ObjectId(targetNote._id) }
+    let filter = { _id: ObjectId(targetNote._id) }
 
-        let result = await collection.remove(filter)
-        client.close()
+    let result = await collection.remove(filter)
+    client.close()
 
-        res.json(result);
-    })
-}
+    res.json(result);
+})
 
-module.exports = settingRoute
+
+module.exports = router;
