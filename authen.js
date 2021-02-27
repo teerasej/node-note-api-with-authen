7c21915792b692c9c269e7453535be25790c3282
Teerasej <teerasej@gmail.com>
2021-02-27 21:52:31 +0700

    spilt strategy to signup and login

--- authen.js
+++ authen.js
@@ -4,26 +4,51 @@ const LocalStrategy = require('passport-local').Strategy;
 const dbConnection = require('./db');
 const dbName = 'nfmongop';
 
-passport.use(new LocalStrategy(
-    async (username, password, done) => {
+passport.use('signup', new LocalStrategy(
+    {
+        emailField: 'email',
+        passwordField: 'password'
+    },
+    async (email, password, done) => {
+        let client = await dbConnection.connect()
+        try {
+
+            let collection = client.db(dbName).collection('users')
+            let user = await collection.insertOne({ email: email, password: password })
+
+            return done(null, user);
+        } catch (error) {
+            return done(null, false, { message: 'create user error' })
+        } finally {
+            client.close()
+        }
+
+    })
+);
+
+passport.use('login', new LocalStrategy(
+    {
+        emailField: 'email',
+        passwordField: 'password'
+    },
+    async (email, password, done) => {
 
         let client = await dbConnection.connect()
         let collection = client.db(dbName).collection('users')
-        let result = await collection.insertOne(newNote);
-        client.close()
-
-        collection.findOne({ username: username },  (err, user) => {
+       
+        collection.findOne({ email: email }, (err, user) => {
             if (err) { return done(err); }
             if (!user) {
-                return done(null, false, { message: 'Incorrect username.' });
+                return done(null, false, { message: 'Incorrect email.' });
             }
             if (!(user.password === password)) {
                 return done(null, false, { message: 'Incorrect password.' });
             }
             return done(null, user);
         });
-    }
-)
+
+        client.close()
+    })
 );
 
 module.exports = passport;
