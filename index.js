5f68753c4033ef77f87d1c58b2254072877fd057
Teerasej <teerasej@gmail.com>
2021-02-27 22:46:58 +0700

    create endpoint for login and generate token if found user in db

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const passport = require('./authen')
 const routeNotes = require('./routes/notes')
+const jwt = require('jsonwebtoken');
 
 const app = express()
 const port = 3000
@@ -23,9 +24,40 @@ app.post(
             message: 'Signup successful',
             user: req.user
         }).status(200)
-    
-
 })
+
+app.post(
+    '/login',
+    async (req, res, next) => {
+      passport.authenticate(
+        'login',
+        async (err, user, info) => {
+          try {
+            if (err || !user) {
+              const error = new Error('An error occurred.');
+  
+              return next(error);
+            }
+  
+            req.login(
+              user,
+              { session: false },
+              async (error) => {
+                if (error) return next(error);
+  
+                const body = { _id: user._id, email: user.email };
+                const token = jwt.sign({ user: body }, 'TOP_SECRET');
+  
+                return res.json({ token });
+              }
+            );
+          } catch (error) {
+            return next(error);
+          }
+        }
+      )(req, res, next);
+    }
+  );
 
 
 app.listen(port, () => console.log(`Note API listening on http://localhost:${port}!`))
