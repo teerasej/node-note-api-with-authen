f311d0b4bc3a05ea659529b25353731750a92185
Teerasej <teerasej@gmail.com>
2021-02-28 08:53:25 +0700

    separate authentication route to its file

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const passport = require('./authen')
-const routeNotes = require('./routes/notes')
-const jwt = require('jsonwebtoken');
+
+const routeNotes = require('./routes/route-note')
+const routeUser = require('./routes/route-user')
+const routeAuthen = require('./routes/route-authen')
 
 const app = express()
 const port = 3000
 
-app.use(express.json());
-app.use(passport.initialize());
-app.use(routeNotes);
+app.use(express.json())
+app.use(passport.initialize())
+app.use('/notes', routeNotes)
+app.use('/users', routeUser)
+app.use('/', routeAuthen)
 
 
 
@@ -16,62 +20,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post(
-    '/signup', 
-    passport.authenticate('signup', { session: false }),
-    async (req, res) => {
-        res.json({
-            message: 'Signup successful',
-            user: req.user
-        }).status(200)
-})
-
-app.post(
-    '/login',
-    async (req, res, next) => {
-      passport.authenticate(
-        'login',
-        async (err, user, info) => {
-          try {
-            if (err || !user) {
-              const error = new Error('An error occurred.');
-  
-              return next(error);
-            }
-  
-            req.login(
-              user,
-              { session: false },
-              async (error) => {
-                if (error) return next(error);
-  
-                const body = { _id: user._id, email: user.email };
-                const token = jwt.sign({ user: body }, 'TOP_SECRET');
-  
-                return res.json({ token });
-              }
-            );
-          } catch (error) {
-            return next(error);
-          }
-        }
-      )(req, res, next);
-    }
-  );
-
-
-  app.get(
-    '/profile',
-    passport.authenticate('jwt', { session: false }),
-    (req, res, next) => {
-      res.json({
-        message: 'You made it to the secure route',
-        user: req.user,
-        token: req.query.secret_token
-      })
-    }
-  );
-  
 
 
 app.listen(port, () => console.log(`Note API listening on http://localhost:${port}!`))
